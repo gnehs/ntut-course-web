4aba8d400836caef5153cad65f4757ac071177cc
gnehs <jayuiop@gmail.com>
2021-01-04 00:24:04 +0800

    updated about

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,29 @@
 <template>
 	<div>
 		<h1>關於</h1>
-		<p>Coming Soon</p>
+		<p>
+			本網站由
+			<a href="https://gnehs.net" target="_blank">勝勝</a> 開發。
+		</p>
+		<h2>開發動機</h2>
+		<p>由於學校課程系統對人類不友善，我希望能有美觀介面與快速響應的課程系統，便製作了此網站。</p>
+		<h2>技術細節</h2>
+		<p>
+			本站資料擷取自
+			<a href="https://aps.ntut.edu.tw/course/tw/course.jsp" target="_blank">國立臺北科技大學課程系統</a>
+			<br />若資料有問題或有任何建議，歡迎發送 Pull Request 或 Issue。
+			<br />
+			<br />網站：
+			<a
+				href="https://github.com/gnehs/ntut-course-web"
+				target="_blank"
+			>https://github.com/gnehs/ntut-course-web</a>
+			<br />爬蟲：
+			<a
+				href="https://github.com/gnehs/ntut-course-crawler"
+				target="_blank"
+			>https://github.com/gnehs/ntut-course-crawler</a>
+		</p>
 	</div>
 </template>
   
