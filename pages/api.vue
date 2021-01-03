61fb80cf2621c7c00e64ca8e4db4783dd123b205
gnehs <jayuiop@gmail.com>
2021-01-03 23:19:42 +0800

    updated api doc

--- pages/api.vue
+++ pages/api.vue
@@ -1,7 +1,131 @@
 <template>
 	<div>
 		<h1>API 手冊</h1>
-		<p>Coming Soon</p>
+		<p>
+			如果你使用 Chrome，可以使用
+			<a
+				href="https://chrome.google.com/webstore/detail/json-formatter/bcjindcccaagfpapjjmafapmmgkkhgoa?hl=zh_tw"
+				target="_blank"
+			>JSON Formatter</a> 來檢視 JSON 檔案
+		</p>
+		<p>API Endpoint: https://gnehs.github.io/ntut-course-crawler/{API}</p>
+		<p>
+			source code:
+			<a
+				href="https://github.com/gnehs/ntut-course-crawler"
+				target="_blank"
+			>https://github.com/gnehs/ntut-course-crawler</a>
+		</p>
+		<h2>
+			GET
+			<code>/main.json</code>
+		</h2>
+		<p>
+			可取得所有可用年份與學期
+			<br />
+			<br />Example:
+			<a
+				href="https://gnehs.github.io/ntut-course-crawler/main.json"
+				target="_blank"
+			>https://gnehs.github.io/ntut-course-crawler/main.json</a>
+		</p>
+		<pre>
+{
+  "109": [
+    1,
+    2
+  ],
+  "108": [
+    1,
+    2
+  ],
+  ...
+}</pre>
+		<h2>
+			GET
+			<code>/{年份}/{學期}/main.json</code>
+		</h2>
+		<p>
+			取得該學期課程資料
+			<br />
+			<br />Example:
+			<a
+				href="https://gnehs.github.io/ntut-course-crawler/109/2/main.json"
+				target="_blank"
+			>https://gnehs.github.io/ntut-course-crawler/109/2/main.json</a>
+			<br />＊請注意：該檔案非常大，可能有機會無法解析
+		</p>
+		<pre>
+[
+  {
+    "id":"281711",
+    "code":"1004002",
+    "name":{
+      "zh":"體育",
+      "en":"Physical Education"
+    },
+    "description":{
+      "zh":"本校體育課程目標為鍛鍊健全身心有專長...",
+      "en":"In second semester, Senior student..."
+    },
+    "stage":"1",
+    "credit":"1.0",
+    "hours":"2",
+    "courseType":"☆",
+    "class":[{
+      "name":"機械四甲",
+      "link":"Subj.jsp?format=-4&year=109&sem=2&code=2171",
+      "code":"2171"
+    }],
+    "teacher":[],
+    "time":{
+      "sun":[],
+      "mon":[],
+      "tue":[],
+      "wed":[],
+      "thu":[],
+      "fri":[],
+      "sat":[]
+    },
+    "classroom":[],
+    "people":"0",
+    "peopleWithdraw":"0",
+    "ta":[],
+    "language":"",
+    "notes":""
+  },
+  ...
+]</pre>
+		<h2>
+			GET
+			<code>/{年份}/{學期}/course/{id}.json</code>
+		</h2>
+		<p>
+			取得課程詳細資料
+			<br />
+			<br />Example:
+			<a
+				href="https://gnehs.github.io/ntut-course-crawler/109/2/course/281841.json"
+				target="_blank"
+			>https://gnehs.github.io/ntut-course-crawler/109/2/course/281841.json</a>
+		</p>
+		<pre>[
+  {
+      "name": "林小媛",
+      "email": "alice_2001@hotmail.com.tw",
+      "latestUpdate": "2020-12-22 16:44:55",
+      "objective": "使學生進一步瞭解首飾設計觀念，...",
+      "schedule": "1. 課程簡介   [第1週]\r\n\t2. 蠟模製...",
+      "materials": "‧「軟蠟變銀飾超簡單」，王美玲著，積木文化...",
+      "foreignLanguageTextbooks": false
+  }
+]</pre>
 	</div>
 </template>
+<style lang="sass" scoped>
+pre
+  background: #ffffff
+  padding: 16px
+  border-radius: 8px
+</style>
   
