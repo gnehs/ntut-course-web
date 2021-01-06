9edf33ad4b4b404436a56568e8c58ac1f4d091ad
gnehs <jayuiop@gmail.com>
2021-01-06 19:15:11 +0800

    changed card hover & active style

--- components/card.vue
+++ components/card.vue
@@ -26,9 +26,9 @@
     &.hoverable
         cursor: pointer
         &:hover
-            box-shadow: 0 10px 20px 0 rgba(0,0,0,var(--vs-shadow-opacity,.05)),0 0px 0px 1px rgba(0,0,0,.05)
+            box-shadow: 0 10px 20px 0 rgba(0,0,0,var(--vs-shadow-opacity,.05)),0 0px 0px 2px rgba(0,0,0,.25)
         &:active
-            box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, var(--vs-shadow-opacity,0)),0 0px 0px 1px rgba(0,0,0,.25)
+            box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, var(--vs-shadow-opacity,0)),0 0px 0px 2px rgba(0,0,0,.5)
             transform: translate(0, 5px)
     p
         margin: 0
