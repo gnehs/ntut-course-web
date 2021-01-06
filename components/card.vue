ec8d5fa5bf25b71b9b96f09b724b723974927265
gnehs <jayuiop@gmail.com>
2021-01-06 16:36:21 +0800

    changed card hover style

--- components/card.vue
+++ components/card.vue
@@ -10,8 +10,7 @@
     color: #2c3e50
     color: rgba(var(--vs-text),1)
     width: 100%
-    box-shadow: 0 5px 20px 0 rgba(0,0,0,.05)
-    box-shadow: 0 5px 20px 0 rgba(0,0,0,var(--vs-shadow-opacity))
+    box-shadow: 0 5px 20px 0 rgba(0,0,0,var(--vs-shadow-opacity,.05)),0 0px 0px 1px rgba(0,0,0,0)
     border-radius: 12px
     transition: all .25s ease
     position: relative
@@ -27,16 +26,14 @@
     &.hoverable
         cursor: pointer
         &:hover
-            box-shadow: 0px 0px 0px 0px rgba(0,0,0,0)
-            box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, var(--vs-shadow-opacity))
-            transform: translate(0, 5px)
-            opacity: .7
+            box-shadow: 0 10px 20px 0 rgba(0,0,0,var(--vs-shadow-opacity,.05)),0 0px 0px 1px rgba(0,0,0,.05)
         &:active
-            opacity: 1
+            box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, var(--vs-shadow-opacity,0)),0 0px 0px 1px rgba(0,0,0,.25)
+            transform: translate(0, 5px)
     p
         margin: 0
         opacity: 0.8
         font-size: 0.85rem
         &+p
             margin-top: 8px
-</style> 
+</style>
