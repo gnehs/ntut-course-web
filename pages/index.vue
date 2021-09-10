f37be4368932cc4a028c06a26d1f0b1027ad62ef
gnehs <jayuiop@gmail.com>
2021-09-10 22:49:28 +0800

    new index header

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,14 @@
 <template>
   <div>
-    <vs-alert :page.sync="alertPage">
-      <template #title>歡迎使用</template>
-
-      <template #page-1>
-        本站資料擷取自
-        <a href="https://aps.ntut.edu.tw/course/tw/course.jsp" target="_blank">國立臺北科技大學課程系統</a
-        >，資料僅供參考，可能會有所遺漏或錯誤，正式資料仍以學校公佈為主。
-      </template>
-      <template #page-2
-        >你可以點選右上方的學期按鈕來切換檢視的學制與學期，目前顯示的資料為
-        {{ `${$store.state.year} 年${$store.state.sem == '1' ? '上' : '下'}學期` }}，{{
-          $store.state.department == 'main' ? '日間部' : $store.state.department
-        }}。</template
-      >
-    </vs-alert>
+    <h1>
+      {{ `${$store.state.year} 年${$store.state.sem == '1' ? '上' : '下'}學期` }} -
+      {{ $store.state.department == 'main' ? '日間部' : $store.state.department }}
+    </h1>
+    <p>
+      本站資料擷取自
+      <a href="https://aps.ntut.edu.tw/course/tw/course.jsp" target="_blank">國立臺北科技大學課程系統</a
+      >，資料僅供參考，可能會有所遺漏或錯誤，正式資料仍以學校公佈為主。
+    </p>
     <br />
     <div class="cards">
       <card class="hoverable padding" :to="`/search?year=${$store.state.year}&sem=${$store.state.sem}&d=${$store.state.department}`">
@@ -53,9 +47,7 @@
   
 <script>
 export default {
-  data: () => ({
-    alertPage: 1
-  }),
+  data: () => ({}),
   created() {},
   methods: {}
 }
