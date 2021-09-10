bb64918a1f6f36d134a870defdc03276283cfe88
gnehs <jayuiop@gmail.com>
2021-09-11 02:26:26 +0800

    added nuxt fallback option

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,6 +57,9 @@ export default {
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
   },
+  generate: {
+    fallback: true
+  },
   router: {
     base: "/ntut-course-web/"
   },
