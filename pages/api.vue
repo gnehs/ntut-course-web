6fa0cd901836a437b8f6e960f9fbb37bcecc5d3d
gnehs <jayuiop@gmail.com>
2022-06-02 01:59:09 +0800

    updated API doc

--- pages/api.vue
+++ pages/api.vue
@@ -2,20 +2,23 @@
 # API 文件
 ## 注意
 - 這不是學校官方提供的 API
-- 這份文件非即時更新，可能會有 API 失效 
+- 這份文件非即時更新，可能會有 API 失效
 - 資料擷取自 [國立臺北科技大學課程系統](https://aps.ntut.edu.tw/course/tw/course.jsp)，資料僅供參考，可能會有所遺漏或錯誤，正式資料仍以學校公佈為主。
 ## 來試試看吧！
 你可以使用 [Hoppscotch](https://hoppscotch.io/?method=GET&url=https://gnehs.github.io&path=/ntut-course-crawler-node/main.json) 或 [Postman](https://www.postman.com/) 來發起請求。
+
+## 相關連結
+[使用了北科課程好朋友 API 的專案](https://github.com/gnehs/ntut-course-crawler-node/issues/1)
 
 -------
 
 ## API Endpoint
 ### Endpoint
-https://gnehs.github.io/ntut-course-crawler-node/  
+https://gnehs.github.io/ntut-course-crawler-node/
 ### 說明
 如果你想取得 `/main.json` 的資料，請將 `Endpoint` 加在其前方。
 
-就像這樣：`https://gnehs.github.io/ntut-course-crawler-node/main.json`  
+就像這樣：`https://gnehs.github.io/ntut-course-crawler-node/main.json`
 
 -------
 # API 清單
@@ -23,13 +26,13 @@ https://gnehs.github.io/ntut-course-crawler-node/
 這裡是與課程相關的 API
 <div class="white-box">
 
-### `/main.json` 取得所有可用之年份與學期 
+### `/main.json` 取得所有可用之年份與學期
 #### Example
 https://gnehs.github.io/ntut-course-crawler-node/main.json
 
 </div><div class="white-box">
 
-### `/{year}/{sem}/{system}.json` 取得某學期某學制課程資料 
+### `/{year}/{sem}/{system}.json` 取得某學期某學制課程資料
 #### 注意
 該檔案非常大，可能有機會無法解析
 #### Example
@@ -40,7 +43,7 @@ https://gnehs.github.io/ntut-course-crawler-node/109/2/進修部.json
 	- `1` 上學期
 	- `2` 下學期
 - `system` 學制
-	- `main` 
+	- `main`
 		- 註：日間部被重新命名為 main
 	- `研究所(日間部、進修部、週末碩士班)`
 	- `進修部`
@@ -88,9 +91,8 @@ https://gnehs.github.io/ntut-course-crawler-node/standards.json
 https://gnehs.github.io/ntut-course-crawler-node/110/standard.json
 #### 參數
 - `year` 民國年
-
 </div>
-</template> 
+</template>
 <style scoped lang="sass">
 img
 	max-width: 100%
@@ -100,10 +102,10 @@ h1
 </style>
 <script>
 export default {
-  head() {
-    return {
-      title: 'API 手冊'
-    }
-  }
+	head() {
+		return {
+			title: 'API 手冊'
+		}
+	}
 }
 </script>
