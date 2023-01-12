44a202b1dfafa7cf9700d2a4e9370b363889fdca
gnehs <jayuiop@gmail.com>
2023-01-12 17:40:19 +0800

    added sitemap generator

--- generateCoursePreview.mjs
+++ generateCoursePreview.mjs
@@ -5,6 +5,7 @@ import { JSDOM } from "jsdom";
 
 
 let now = new Date()
+let sitemapUrls = []
 
 let dom = await JSDOM.fromFile('./dist/200.html')
 let document = dom.window.document
@@ -13,7 +14,6 @@ let yearSems = await axios.get('https://gnehs.github.io/ntut-course-crawler-node
 let tasks = Object.entries(yearSems.data)
   .map((([year, sems]) => sems.map(sem => ({ year, sem }))))
   .flat()
-  // .slice(-6)
   .map(async ({ year, sem }) => {
     let courses = await axios.get(`https://gnehs.github.io/ntut-course-crawler-node/${year}/${sem}/main.json`)
     console.log(`Generate routes: ${year}/${sem} ${courses.data.length}`)
@@ -26,6 +26,7 @@ let tasks = Object.entries(yearSems.data)
       let title = `${parsedCourseId} ${course.name.zh}`
       let description = `${course.description.zh}`
       let image = `https://ntut-course-og.gnehs.net/api?year=${year}&sem=${sem}&id=${course.id}`
+      let url = `https://ntut-course.gnehs.net/course/${year}/${sem}/${course.id}`
 
 
       function setMeta(name, content) {
@@ -40,7 +41,7 @@ let tasks = Object.entries(yearSems.data)
       }
       setMeta('og:title', title)
       setMeta('og:description', description)
-      setMeta('og:url', `https://ntut-course.gnehs.net/course/${year}/${sem}/${course.id}`)
+      setMeta('og:url', url)
       setMeta('og:image', image)
       setMeta('description', description)
       setMeta('twitter:title', title)
@@ -50,7 +51,20 @@ let tasks = Object.entries(yearSems.data)
       document.querySelector('title').textContent = title
 
       fs.writeFileSync(`./dist/course/${year}/${sem}/${course.id}.html`, dom.serialize().replace(/&amp;/g, '&'))
+      sitemapUrls.push(url)
     })
   })
 await Promise.all(tasks)
 console.log(`Generate routes: ${((new Date() - now) / 1000).toFixed(2)}s`)
+
+//https://stackoverflow.com/questions/8495687/split-array-into-chunks
+Object.defineProperty(Array.prototype, 'chunk', {
+  value: function (n) {
+    let ceil = Math.ceil;
+    return Array.from(Array(ceil(this.length / n)), (_, i) => this.slice(i * n, i * n + n));
+  }
+});
+sitemapUrls.chunk(50000).forEach((chunk, index) => {
+  fs.writeFileSync(`./dist/sitemap-${index + 1}.txt`, chunk.join('\n'), { encoding: 'utf8' })
+})
+console.log(`${sitemapUrls.length} urls in sitemap`)
